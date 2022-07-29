dd98e1ab07d81c886b022d0b40e9143a72ac2dbb
Leonardo Lopes da Luz <leonardolluzti@gmail.com>
2022-07-29 03:42:37 -0300

    Adicionadas funções de saldo, deposito, saque e transferência

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Aplicativo de Banco feito em Python e banco de dados SQLite.
 
 Funções: 
-- Inserir, editar, listar, pesquisar e deletar clientes
-- Inserir, editar, listar, pesquisar e deletar contas
+- Inserir, editar, listar, pesquisar e deletar clientes e contas.
+- Consultar saldo, fazer depósitos, fazer saques e transferências entre contas.
 
 ## Tecnologias utilizadas:
 - Python 3.8.10
--- banco.py
+++ banco.py
@@ -62,6 +62,60 @@ class BancoDB:
 
         for linha in self.cursor.fetchall():
             print(linha)
+    
+    def exbirSaldo(self, valor):
+        consulta = 'SELECT saldo FROM conta WHERE numero LIKE ?'
+        self.cursor.execute(consulta, (f'%{valor}%',))
+        self.data = self.cursor.fetchone()
+        print("R$ %s" % self.data)
+
+    def depositar(self, valor1, valor2):
+        consulta1 = 'SELECT saldo FROM conta WHERE numero LIKE ?'
+        self.cursor.execute(consulta1, (f'%{valor1}%',))
+        self.data = self.cursor.fetchone()
+        print("Saldo anterior R$ %s" % self.data)
+        numero = valor1
+        saldo = float(self.data[0]) + float(valor2)
+        consulta2 = 'UPDATE OR IGNORE conta SET saldo=? WHERE numero=?'
+        self.cursor.execute(consulta2, (saldo, numero))
+        self.conn.commit()
+        consulta3 = 'SELECT saldo FROM conta WHERE numero LIKE ?'
+        self.cursor.execute(consulta3, (f'%{valor1}%',))
+        self.data = self.cursor.fetchone()
+        print("Saldo atual R$ %s" % self.data)        
+
+    def sacar(self, valor1, valor2):
+        consulta1 = 'SELECT saldo FROM conta WHERE numero LIKE ?'
+        self.cursor.execute(consulta1, (f'%{valor1}%',))
+        self.data = self.cursor.fetchone()
+        print("Saldo anterior R$ %s" % self.data)
+        numero = valor1
+        s1 = float(self.data[0])
+        s2 = float(valor2)
+        if (s1>=s2 and s2>0):
+            saldo = s1 - s2
+            consulta2 = 'UPDATE OR IGNORE conta SET saldo=? WHERE numero=?'
+            self.cursor.execute(consulta2, (saldo, numero))
+            self.conn.commit()
+            consulta3 = 'SELECT saldo FROM conta WHERE numero LIKE ?'
+            self.cursor.execute(consulta3, (f'%{valor1}%',))
+            self.data = self.cursor.fetchone()
+            print("Saldo atual R$ %s" % self.data)   
+        else:
+            print("Valor informado não é válido!")
+    def transferir(self, agencia, conta2, tipo, valor):
+        print("Transferência realizada com sucesso!")
+        print("Dados da transferência:")
+        print("Agência : " + agencia)
+        print("Conta : " + conta2)
+        if(tipo=='1'):
+            print("Tipo : 1 - Conta Corrente")
+        elif(tipo=='2'):
+            print("Tipo : 2 - Conta Poupança")
+        elif(tipo=='3'):
+            print("Tipo : 3 - Conta Salário")
+        else: 
+            print("Tipo de conta não encontrado")
 
     def fechar(self):
         self.cursor.close()
@@ -71,7 +125,7 @@ class BancoDB:
 if __name__ == '__main__':
     banco = BancoDB('banco.db')
     menu = '0'
-    while menu != '11':
+    while menu != '15':
         print ("========================")        
         print ("| 1 - Mostrar Clientes |")
         print ("| 2 - Buscar Cliente   |")
@@ -85,7 +139,12 @@ if __name__ == '__main__':
         print ("| 9 - Editar Conta     |")
         print ("| 10 - Excluir Conta   |")
         print ("========================")
-        print ("| 11 - Sair do Banco   |")
+        print ("| 11 - Exibir Saldo    |")
+        print ("| 12 - Depositar       |")
+        print ("| 13 - Sacar           |")
+        print ("| 14 - Transferir      |")
+        print ("========================")
+        print ("| 15 - Sair do Banco   |")
         print ("========================")
         menu = input("Digite uma opção: ")
         # Operações com clientes: #
@@ -123,9 +182,9 @@ if __name__ == '__main__':
             agencia = input("Digite a agencia: ")
             numero = input("Digite o numero da conta: ")
             print("Tipos de conta:")
-            print("1 - Conta Corrente:")
-            print("2 - Conta Poupança:")
-            print("3 - Conta Salário:")
+            print("1 - Conta Corrente")
+            print("2 - Conta Poupança")
+            print("3 - Conta Salário")
             tipo = input("Digite o tipo de conta:  ")
             saldo = input("Digite o saldo: ")
             banco.inserirConta(agencia, numero, tipo, saldo, idCliente)
@@ -135,9 +194,9 @@ if __name__ == '__main__':
             agencia = input("Digite a agencia: ")
             numero = input("Digite o numero da conta: ")
             print("Tipos de conta:")
-            print("1 - Conta Corrente:")
-            print("2 - Conta Poupança:")
-            print("3 - Conta Salário:")
+            print("1 - Conta Corrente")
+            print("2 - Conta Poupança")
+            print("3 - Conta Salário")
             tipo = input("Digite o tipo de conta:  ")
             saldo = input("Digite o saldo: ")
             banco.editarConta(agencia, numero, tipo, saldo, id)
@@ -146,5 +205,40 @@ if __name__ == '__main__':
             id = input("Digite o id da conta que deseja excluir: ")
             banco.excluirConta(id)
         elif menu == '11': 
+            print("Saldo")
+            print("=====")
+            numero = input("Digite o numero da conta: ")
+            banco.exbirSaldo(numero)
+        # Depósito #
+        elif menu == '12': 
+            print("Depósito")
+            print("========")
+            numero = input("Digite o numero da conta: ")
+            valor = input("Digite o valor do depósito: R$ ")
+            banco.depositar(numero, valor)
+        # Saque #
+        elif menu == '13': 
+            print("Saque")
+            print("=====")
+            numero = input("Digite o numero da conta: ")
+            valor = input("Digite o valor do saque: R$ ")
+            banco.sacar(numero, valor)
+        # Transferêcia #
+        elif menu == '14': 
+            print("Transferêcia")
+            print("============")
+            conta1 = input("Digite o numero da conta de origem: ")
+            agencia = input("Digite a agência da conta de destino: ")
+            conta2 = input("Digite o numero da conta de destino: ")
+            print("Tipos de conta:")
+            print("1 - Conta Corrente")
+            print("2 - Conta Poupança")
+            print("3 - Conta Salário")
+            tipo = input("Digite o tipo da conta de destino: ")
+            valor = input("Digite o valor da transferência: R$ ")
+            banco.sacar(conta1, valor)
+            banco.depositar(conta2, valor)
+            banco.transferir(agencia, conta2, tipo, valor)
+        elif menu == '15': 
             banco.fechar()
             print ("Saiu do Banco - Volte Sempre!")
